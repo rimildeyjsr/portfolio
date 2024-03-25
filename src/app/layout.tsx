05988c44b08b9d0935e3fd615262b36d0038c9fb
Rimil Dey <rimildeyjsr@gmail.com>
2024-03-25 12:45:23 +0530

    Push all code to girhub

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Rimil Dey',
+    default:
+      'Rimil Dey - Front-end engineer',
+  },
+  description:
+    'Hi there! I am a front-end engineer with 5 years of experience. I’ve delivered several impactful and innovative products using React & Angular! I currently work for the engineering team at Paradime Labs',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
