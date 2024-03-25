01b5e0c47fc9617824d046cd6016895c8904dcbf
Rimil Dey <rimildeyjsr@gmail.com>
2024-03-25 18:20:41 +0530

    Projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -10,34 +10,53 @@ import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
 
 const projects = [
   {
-    name: 'Animaginary',
+    name: 'Patterns - Playground app',
     description:
-      'High performance web animation library, hand-written in optimized WASM.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoAnimaginary,
+      'Swift',
+    link: { href: 'https://github.com/rimildeyjsr/patterns', label: 'github.com' },
   },
   {
-    name: 'HelioStream',
+    name: "St. Anthony's school website",
     description:
-      'Real-time video streaming library, optimized for interstellar transmission.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoHelioStream,
+      'HTML, CSS, Jacascript, Jquery',
+    link: { href: 'https://github.com/rimildeyjsr/st-anthony-website', label: 'github.com' },
   },
   {
-    name: 'cosmOS',
+    name: "Prateek's portfolio website",
     description:
-      'The operating system that powers our Planetaria space shuttles.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoCosmos,
+      'HTML, CSS, Jacascript, Jquery',
+    link: { href: 'https://github.com/rimildeyjsr/Project-Portfolio', label: 'github.com' },
   },
   {
-    name: 'OpenShuttle',
+    name: 'Admin dashboard',
     description:
-      'The schematics for the first rocket I designed that successfully made it to orbit.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoOpenShuttle,
+      'React, Firebase',
+    link: { href: 'https://github.com/rimildeyjsr/admin-dashboard', label: 'github.com' },
   },
 ]
+
+const openSourceContributions = [
+  {
+    name: 'Lexical (Meta)',
+    link: { href: 'https://github.com/facebook/lexical/pull/1873', label: 'github.com' },
+  },
+  {
+    name: 'Operational UI (Contiamo)',
+    link: { href: 'https://github.com/contiamo/operational-ui/pull/1342', label: 'github.com' },
+  },
+  {
+    name: 'Reactist (Doist)',
+    link: { href: 'https://github.com/Doist/reactist/pull/268', label: 'github.com'},
+  },
+  {
+    name: 'Fast (Microsoft)',
+    link: { href: 'https://github.com/microsoft/fast/pull/3730', label: 'github.com'},
+  },
+  {
+    name: 'Operational UI (Contiamo)',
+    link: { href: 'https://github.com/contiamo/operational-ui/pull/1125', label: 'github.com'},
+  }
+];
 
 function LinkIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
@@ -58,34 +77,51 @@ export const metadata: Metadata = {
 export default function Projects() {
   return (
     <SimpleLayout
-      title="Things I’ve made trying to put my dent in the universe."
-      intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+      title="Open source contributions and Projects"
+      intro=""
     >
-      <ul
-        role="list"
-        className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
-      >
-        {projects.map((project) => (
-          <Card as="li" key={project.name}>
-            <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-              <Image
-                src={project.logo}
-                alt=""
-                className="h-8 w-8"
-                unoptimized
-              />
-            </div>
-            <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-              <Card.Link href={project.link.href}>{project.name}</Card.Link>
-            </h2>
-            <Card.Description>{project.description}</Card.Description>
-            <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-              <LinkIcon className="h-6 w-6 flex-none" />
-              <span className="ml-2">{project.link.label}</span>
-            </p>
-          </Card>
-        ))}
-      </ul>
+      <div className="grid grid-cols-1 divide-y">
+        <div className="mb-16 py-2 first:pt-0">
+          <h3 className="font-bold mb-8">Open source contributions</h3>
+          <ul
+            role="list"
+            className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
+          >
+            {openSourceContributions.map((project) => (
+              <Card as="li" key={project.name}>
+                <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
+                  <Card.Link href={project.link.href}>{project.name}</Card.Link>
+                </h2>
+                <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+                  <LinkIcon className="h-6 w-6 flex-none" />
+                  <span className="ml-2">{project.link.label}</span>
+                </p>
+              </Card>
+            ))}
+          </ul>
+        </div>
+
+        <div className="py-16 last:pb-0">
+          <h3 className="font-bold mb-8">Projects</h3>
+          <ul
+            role="list"
+            className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
+          >
+            {projects.map((project) => (
+              <Card as="li" key={project.name}>
+                <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
+                  <Card.Link href={project.link.href}>{project.name}</Card.Link>
+                </h2>
+                <Card.Description>{project.description}</Card.Description>
+                <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+                  <LinkIcon className="h-6 w-6 flex-none" />
+                  <span className="ml-2">{project.link.label}</span>
+                </p>
+              </Card>
+            ))}
+          </ul>
+        </div>
+      </div>
     </SimpleLayout>
   )
 }
